eedaf2048fb0a9171c801d19007ae1c2994212d4
Jakob Bjørnskov <jakob.bjornskov@me.com>
2022-08-05 14:38:18 +0200

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup
+import setuptools
 setup(
     name="twin4build",
     python_requires='<3.8',
     version="0.0.0",
     description="A library and framework for modeling and creating Digital Twins of buildings.",
-    url="https://github.com/JBjoernskov/Ifc2Graph",
+    url="https://github.com/JBjoernskov/Twin4Build",
     keywords="Digital Twins, Energy Modeling, Building Performance Simulation",
     author="Jakob Bjørnskov, Center for Energy Informatics SDU",
     author_email="jakob.bjornskov@me.com, jabj@mmmi.sdu.dk",
     license="BSD",
     platforms=["Windows", "Linux"],
-    packages=[
-        "saref",
-        "saref4bldg",
-        "saref4syst"
-    ],
+    packages=setuptools.find_packages()
     include_package_data=True,
     install_requires=[
         "matplotlib",
