7287af018da6f630f9c0020dc7fbbe75404700af
Jakob Bjørnskov <jakob.bjornskov@me.com>
2023-01-04 20:30:39 +0100

    Push before branching

--- twin4build/optimizer/optimizer.py
+++ twin4build/optimizer/optimizer.py
@@ -8,7 +8,7 @@ Pytorch automatic differentiation could be used.
 For some period:
 
 0. Initialize inputs as torch.Tensors with "<variable>.requires_grad=true". 
-All other parameters has "<parameter>.requires_grad=false". 
+All parameters are frozen with "<parameter>.requires_grad=false". 
 
 Repeat 1-3 until convergence or stop criteria
 
@@ -23,9 +23,12 @@ Monte Carlo discrete optimization could also be used.
 "Monte-Carlo Tree Search for Policy Optimization"
 https://www.ijcai.org/proceedings/2019/0432.pdf
 
-legal_actions: {variable: [a1, a2, ... an]}
-lower_bounds: {variable: 0}
-upper_bounds: {variable: 999}
+legal_actions: {variable1: [a1, a2, ... an]
+                variable2: [a1, a2, ... an]}
+lower_bounds: {variable1: 0,
+                variable2: 0}
+upper_bounds: {variable1: 999,
+                variable2: 999}
 
 model, decision_variables, legal_actions, lower_bound, upper_bound
 
