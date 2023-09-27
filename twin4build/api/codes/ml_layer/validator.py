f34d1acd5745e8a66423cee013d0d0e4750acf8a
Avneet <avneet@india.nec.com>
2023-09-27 03:31:40 +0000

    Warm Up logic
    We are adding warm up logic here with some minor code updates like new validator code and docker files.

---
+++ twin4build/api/codes/ml_layer/validator.py
@@ -0,0 +1,54 @@
+
+from twin4build.logger.Logging import Logging
+
+#from twin4build.api.codes.ml_layer.simulator_api import SimulatorAPI
+
+# Initialize the logger
+logger = Logging.get_logger('API_logfile')
+
+class Validator:
+    def __init__(self):
+        pass
+
+    
+    def validate_input_data(self,input_data):
+            try:
+                if len(input_data['inputs_sensor'])  < 1 :
+                    return False
+                 
+                # getting the dmi inputs from the ml_inputs dict
+                dmi = input_data['inputs_sensor']['ml_inputs_dmi']
+                ml_i = input_data['inputs_sensor']['ml_inputs']
+                # checking for the start time in metadata and observed values in the dmi inputs 
+                if(input_data["metadata"]['start_time'] == '') or ('damper' not in ml_i.keys()) or ('observed' not in dmi.keys()):
+                    logger.error("Invalid input data got")
+                    return False
+                else:
+                    return True
+                
+            except Exception as input_data_valid_error:
+                logger.error('An error has occured while validating input data ',input_data_valid_error)
+                return False
+    
+    def validate_response_data(self,reponse_data):
+        try :
+            #check if response data is None 
+            if(reponse_data is None or reponse_data == {}):
+                logger.error("No data came from the database , no table got maybe ")
+                return False
+            
+            # searching for the time key in the reponse data else returning false
+            if("time" not in reponse_data.keys()):
+                logger.error("Invalid response data ")
+                return False
+            
+            #checking the time list is null then returning false
+            elif (len(reponse_data['time']) < 1):
+                logger.error("Invalid response data ")
+                return False
+            else:
+                return True
+            
+        except Exception as response_data_valid_error:
+            logger.error('An error has occured while validating response data ',response_data_valid_error)
+            return False
