8b2ce5fac10842691bd48979cade69146a06e44d
Avneet <avneet@india.nec.com>
2023-09-29 16:42:39 +0000

    code updates
    Few minor code updates .

--- twin4build/api/codes/database/db_data_handler.py
+++ twin4build/api/codes/database/db_data_handler.py
@@ -11,6 +11,8 @@ from twin4build.logger.Logging import Logging
 from twin4build.config.Config import ConfigReader
 import os
 import sys
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import insert
 
 from sqlalchemy import create_engine, Column, String, TEXT, DateTime, Integer, Float, JSON, BIGINT, BigInteger
 from sqlalchemy.orm import sessionmaker
@@ -195,6 +197,7 @@ class db_connector:
             self.engine.dispose()
             logger.info("Connection to PostgresSQL closed Successfully")
             print("Connection to PostgreSQL closed successfully.")
+
         except Exception as e:
             logger.error(f"Error disconnecting from PostgreSQL: {e}")
             print(f"Error disconnecting from PostgreSQL: {e}")
@@ -219,6 +222,11 @@ class db_connector:
             #self.session.close()
             '''
 
+            if len(inputs) < 1:
+                logger.error("Empty data got for entering to database")
+
+                return None
+
             self.session.bulk_insert_mappings(self.tables[table_name],inputs)
             self.session.commit()
             self.session.close()
@@ -240,9 +248,11 @@ class db_connector:
             list: A list of queried data from the specified table.
         """
         queried_data = []
+
         try:
             queried_data = self.session.query(self.tables[table_name]).order_by(
                 desc(self.tables[table_name].time_index)).all()
+            
             self.session.close()
             logger.info(f"{table_name} retrieved from the database")
             print(f"{table_name} retrieved from database")
