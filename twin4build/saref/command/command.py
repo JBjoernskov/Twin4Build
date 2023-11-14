56dacf9e5c450d9e14caec3c0a8e10bfd1915bf7
Jakob Bjørnskov <jakob.bjornskov@me.com>
2023-11-14 13:53:57 +0100

    Fix bug in command.py

--- twin4build/saref/command/command.py
+++ twin4build/saref/command/command.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from typing import Union
 import twin4build.saref.function.function as function
 import twin4build.saref.state.state as state
-import twin4build.saref.date_time.date_time as date_time
 class Command:
     def __init__(self,
                 actsUpon: Union[state.State, None]=None,
@@ -10,5 +9,5 @@ class Command:
                 **kwargs):
         
         assert isinstance(actsUpon, state.State) or actsUpon is None, "Attribute \"actsUpon\" is of type \"" + str(type(actsUpon)) + "\" but must be of type \"" + str(state.State) + "\""
-        assert isinstance(isCommandOf, date_time.DateTime) or isCommandOf is None, "Attribute \"isCommandOf\" is of type \"" + str(type(isCommandOf)) + "\" but must be of type \"" + str(function.Function) + "\""
+        assert isinstance(isCommandOf, function.Function) or isCommandOf is None, "Attribute \"isCommandOf\" is of type \"" + str(type(isCommandOf)) + "\" but must be of type \"" + str(function.Function) + "\""
 
