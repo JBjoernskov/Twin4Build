d1e3fa4f7f10226eb3867261c345d63097e3a21e
Jakob Bjørnskov <jakob.bjornskov@me.com>
2023-11-22 14:37:06 +0100

    Fix class name bug in test

--- twin4build/examples/test_examples.py
+++ twin4build/examples/test_examples.py
@@ -3,7 +3,7 @@ from twin4build.examples.minimal_example import minimal_example
 from twin4build.examples.space_co2_no_controller_example import space_co2_no_controller_example
 from twin4build.examples.space_co2_controller_example import space_co2_controller_example
 
-class TestOU44RoomCase(unittest.TestCase):
+class TestExamples(unittest.TestCase):
     @unittest.skipIf(False, 'Currently not used')
     def test():
         minimal_example()
