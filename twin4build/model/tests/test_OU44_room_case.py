940d9cd168cbc6e518f44a178670c5a5341f9af9
Jakob Bjørnskov <jakob.bjornskov@me.com>
2023-11-28 16:19:31 +0100

    Add DMI weather file for tests

--- twin4build/model/tests/test_OU44_room_case.py
+++ twin4build/model/tests/test_OU44_room_case.py
@@ -111,7 +111,7 @@ class TestOU44RoomCase(unittest.TestCase):
         # startPeriod = datetime.datetime(year=2022, month=10, day=23, hour=0, minute=0, second=0)
         # endPeriod = datetime.datetime(year=2022, month=11, day=6, hour=0, minute=0, second=0)
         startPeriod = datetime.datetime(year=2022, month=1, day=3, hour=0, minute=0, second=0, tzinfo=tz.gettz("Europe/Copenhagen")) #piecewise 20.5-23
-        endPeriod = datetime.datetime(year=2022, month=1, day=30, hour=0, minute=0, second=0, tzinfo=tz.gettz("Europe/Copenhagen")) #piecewise 20.5-23
+        endPeriod = datetime.datetime(year=2022, month=1, day=5, hour=0, minute=0, second=0, tzinfo=tz.gettz("Europe/Copenhagen")) #piecewise 20.5-23
         # startPeriod = datetime.datetime(year=2022, month=1, day=1, hour=0, minute=0, second=0) #piecewise 20.5-23
         # endPeriod = datetime.datetime(year=2022, month=2, day=1, hour=0, minute=0, second=0) #piecewise 20.5-23
         # Model.extend_model = extend_model
