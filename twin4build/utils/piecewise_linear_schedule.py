e634fc94b88500a49fa97d945123e3d51721894e
JBjoernskov <jakob.bjornskov@me.com>
2024-05-08 13:34:01 +0200

    Fix issue in TimeSeriesInput

--- twin4build/utils/piecewise_linear_schedule.py
+++ twin4build/utils/piecewise_linear_schedule.py
@@ -34,18 +34,6 @@ class PiecewiseLinearScheduleSystem(PiecewiseLinearSystem, ScheduleSystem):
     def config(self):
         return self._config
 
-    def cache(self,
-            startTime=None,
-            endTime=None,
-            stepSize=None):
-        pass
-
-    def initialize(self,
-                    startTime=None,
-                    endTime=None,
-                    stepSize=None):
-        pass
-
     def do_step(self, secondTime=None, dateTime=None, stepSize=None):
         '''
              takes in the current time and uses the calibrated model to make a prediction for the output value
