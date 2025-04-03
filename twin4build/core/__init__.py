4ea3aa4851612192e3f6d5564dd7d2b7e0fc16a6
JBjoernskov <jakob.bjornskov@me.com>
2025-04-03 15:30:04 +0200

    Finish implementation of Translator and milp solver

--- twin4build/core/__init__.py
+++ twin4build/core/__init__.py
@@ -1,7 +1,3 @@
-import rdflib
-
-
-from twin4build.utils.lazy_loader import LazyLoader
 from twin4build.systems.saref4syst.system import System
 from twin4build.systems.saref4syst.connection import Connection
 from twin4build.systems.saref4syst.connection_point import ConnectionPoint
@@ -16,48 +12,8 @@ from twin4build.monitor.monitor import Monitor
 from twin4build.estimator.estimator import Estimator
 from twin4build.evaluator.evaluator import Evaluator
 from twin4build.translator.translator import Translator
-
-
-
-# Create a single lazy loader instance
-# _lazy = LazyLoader()
-
-# # Register lazy classes
-# _lazy.add_lazy_class("System", "twin4build.systems.saref4syst.system", "System")
-# _lazy.add_lazy_class("Connection", "twin4build.systems.saref4syst.connection", "Connection")
-# _lazy.add_lazy_class("ConnectionPoint", "twin4build.systems.saref4syst.connection_point", "ConnectionPoint")
-# _lazy.add_lazy_class("Model", "twin4build.model.model", "Model")
-# _lazy.add_lazy_class("SemanticModel", "twin4build.model.semantic_model.semantic_model", "SemanticModel")
-# _lazy.add_lazy_class("SemanticObject", "twin4build.model.semantic_model.semantic_model", "SemanticObject")
-# _lazy.add_lazy_class("SemanticType", "twin4build.model.semantic_model.semantic_model", "SemanticType")
-# _lazy.add_lazy_class("SemanticProperty", "twin4build.model.semantic_model.semantic_model", "SemanticProperty")
-# _lazy.add_lazy_class("SimulationModel", "twin4build.model.simulation_model", "SimulationModel")
-# _lazy.add_lazy_class("Simulator", "twin4build.simulator.simulator", "Simulator")
-# _lazy.add_lazy_class("Monitor", "twin4build.monitor.monitor", "Monitor")
-# _lazy.add_lazy_class("Estimator", "twin4build.estimator.estimator", "Estimator")
-# _lazy.add_lazy_class("Evaluator", "twin4build.evaluator.evaluator", "Evaluator")
-# _lazy.add_lazy_class("Translator", "twin4build.translator.translator", "Translator")
-
-# # Export classes through the module
-# System = _lazy.System
-# Connection = _lazy.Connection
-# ConnectionPoint = _lazy.ConnectionPoint
-# Model = _lazy.Model
-# SemanticModel = _lazy.SemanticModel
-# SemanticObject = _lazy.SemanticObject
-# SemanticType = _lazy.SemanticType
-# SemanticProperty = _lazy.SemanticProperty
-# SimulationModel = _lazy.SimulationModel
-# Simulator = _lazy.Simulator
-# Monitor = _lazy.Monitor
-# Estimator = _lazy.Estimator
-# Evaluator = _lazy.Evaluator
-# Translator = _lazy.Translator
-
-
-# from types import NoneType
 NoneType = type(None)
-
+import rdflib
 
 FSO = rdflib.Namespace("https://w3id.org/fso#")
 SAREF = rdflib.Namespace("https://saref.etsi.org/core/")
