bc9e5e30ef4709595b15d46c843e7e79bbb86268
Jakob Bjørnskov <jakob.bjornskov@me.com>
2025-10-24 13:02:32 +0200

    Make TimeSeriesInputSystem support additional batch dimension

--- twin4build/systems/utils/time_series_input_system.py
+++ twin4build/systems/utils/time_series_input_system.py
@@ -5,11 +5,12 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 # Third party imports
 import pandas as pd
+import numpy as np
 
 # Local application imports
 import twin4build.core as core
 import twin4build.utils.types as tps
-from twin4build.utils.data_loaders.load import load_from_database, load_from_spreadsheet
+from twin4build.utils.data_loaders.load import load_from_database, load_from_spreadsheet, sample_from_df
 from twin4build.utils.get_main_dir import get_main_dir
 
 
@@ -72,8 +73,15 @@ class TimeSeriesInputSystem(core.System):
             df is not None or filename is not None
         ), 'Either "df" or "filename" must be provided as argument.'
 
-        # Store attributes as private variables
-        self._df = df
+        # # Store attributes as private variables
+        # if isinstance(df, pd.DataFrame):
+        #     df = [df]
+        # else:
+        #     assert isinstance(df, [list, type(None)]), "df must be a pandas DataFrame or a list of pandas DataFrames"
+        #     if df is None:
+        #         df = []
+        self._df_init = df
+        self.df = []
         self._useSpreadsheet = useSpreadsheet
         self._useDatabase = useDatabase
         self._filename = filename
@@ -82,7 +90,7 @@ class TimeSeriesInputSystem(core.System):
         self._uuid = uuid
         self._name = name
         self._dbconfig = dbconfig
-        self._cached_initialize_arguments = None
+        self._cached_initialize_arguments = []
         self._cache_root = get_main_dir()
 
         # Define inputs and outputs as private variables
@@ -272,10 +280,9 @@ class TimeSeriesInputSystem(core.System):
 
     def initialize(
         self,
-        start_time: datetime.datetime,
-        end_time: datetime.datetime,
-        step_size: int,
-        simulator: core.Simulator,
+        start_time: List[datetime.datetime],
+        end_time: List[datetime.datetime],
+        step_size: List[int],
     ) -> None:
         """
         Initialize the TimeSeriesInputSystem.
@@ -284,42 +291,71 @@ class TimeSeriesInputSystem(core.System):
             start_time (datetime.datetime): Start time for the simulation.
             end_time (datetime.datetime): End time for the simulation.
             step_size (int): Step size for the simulation.
-            simulator (core.Simulator): Simulator to be used for initialization.
         """
-        if self.df is None or (
-            self._cached_initialize_arguments != (start_time, end_time, step_size)
-            and self._cached_initialize_arguments is not None
-        ):
-            if self.useSpreadsheet:
-                self.df = load_from_spreadsheet(
-                    self.filename,
-                    self.datecolumn,
-                    self.valuecolumn,
-                    step_size=step_size,
-                    start_time=start_time,
-                    end_time=end_time,
-                    cache_root=self._cache_root,
-                )
-            elif self.useDatabase:
-                self.df = load_from_database(
-                    config=self.dbconfig,
-                    sensor_uuid=self.uuid,
-                    sensor_name=self.name,
-                    step_size=step_size,
-                    start_time=start_time,
-                    end_time=end_time,
-                    cache_root=self._cache_root,
-                )
+        # 
+        if len(self._cached_initialize_arguments)>0 and len(self._cached_initialize_arguments) == len(start_time): # Only check first element of tuple for length as all elements are the same length
+            is_cached = all(start_time_==c[0] and end_time_==c[1] and step_size_==c[2] for start_time_, end_time_, step_size_, c in zip(start_time, end_time, step_size, self._cached_initialize_arguments))
+        else:
+            is_cached = False
+
+
+
+        if is_cached==False:
+            self.df = []
+            self._cached_initialize_arguments = []
+            for start_time_, end_time_, step_size_ in zip(start_time, end_time, step_size):
+                # if (start_time_, end_time_, step_size_) not in self._cached_initialize_arguments:
+
+                if self._df_init is None:
+                    if self.useSpreadsheet:
+                        df = load_from_spreadsheet(
+                            self.filename,
+                            self.datecolumn,
+                            self.valuecolumn,
+                            step_size=step_size_,
+                            start_time=start_time_,
+                            end_time=end_time_,
+                            cache_root=self._cache_root,
+                        )
+                    elif self.useDatabase:
+                        df = load_from_database(
+                            config=self.dbconfig,
+                            sensor_uuid=self.uuid,
+                            sensor_name=self.name,
+                            step_size=step_size_,
+                            start_time=start_time_,
+                            end_time=end_time_,
+                            cache_root=self._cache_root,
+                        )
+                else:
+                    df_ = self._df_init.copy()
+                    df_.reset_index(inplace=True)
+                    df = sample_from_df(
+                        df_,
+                        datecolumn=0,
+                        valuecolumn=1,
+                        step_size=step_size_,
+                        start_time=start_time_,
+                        end_time=end_time_,
+                    )
+                    
+
+                self._cached_initialize_arguments.append((start_time_, end_time_, step_size_))
+                self.df.append(df)
+
+        _, _, n_timesteps = core.Simulator.get_simulation_timesteps(start_time, end_time, step_size)
+        values = np.empty((len(self.df), n_timesteps))
+        values.fill(np.nan)
+        for batch_index, df in enumerate(self.df):
+            size = len(df.index)
+            values[batch_index,:size] = df.values[:,0]
 
         self.output["value"].initialize(
-                start_time=start_time,
-                end_time=end_time,
-                step_size=step_size,
-                simulator=simulator,
-                values=self.df.values[:,0],
+                n_timesteps,
+                batch_size=len(start_time),
+                values=values,
             )
 
-        self._cached_initialize_arguments = (start_time, end_time, step_size)
 
     def do_step(
         self,
