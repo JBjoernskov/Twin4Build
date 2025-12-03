1115130535f6885538cf56489e450e5c7049252e
Jakob Bjørnskov <jakob.bjornskov@me.com>
2025-12-03 09:26:00 +0100

    Add self.initialize() in Sensor.get_physical_readings()

--- twin4build/systems/sensor/sensor_system.py
+++ twin4build/systems/sensor/sensor_system.py
@@ -756,6 +756,7 @@ class SensorSystem(core.System):
         Raises:
             AssertionError: If called on a virtual sensor (no physical readings available).
         """
+        self.initialize(start_time, end_time, step_size)
         assert (
             self.time_series_input is not None
         ), f'Cannot return physical readings for Sensor with id "{self.id}" as the argument "filename" was not provided when the object was initialized.'
