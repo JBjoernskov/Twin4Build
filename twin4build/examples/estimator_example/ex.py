a7ff904aef66b1fc858dc19c273a0f3e21c4b394
Jakob Bjørnskov <jakob.bjornskov@me.com>
2025-12-11 06:46:36 +0100

    Mockup example of twin4build workflow

---
+++ twin4build/examples/estimator_example/ex.py
@@ -0,0 +1,34 @@
+import twin4build as tb
+
+
+"""
+MODEL CREATION
+"""
+model = tb.Model(id="example") # Create a model
+c1 = tb.SpaceHeaterTorchSystem(...) # Create a space heater
+c2 = tb.BuildingSpaceTorchSystem(...) # Create a building space
+model.add_connection(c1, c2, "Power", "heatGain") # Add a connection between the space heater and the building space
+model.load() # Load the model
+
+"""
+SIMULATION
+"""
+simulator = tb.Simulator(model) # Create a simulator
+simulator.simulate(...) # Run the simulator
+
+"""
+ESTIMATION
+"""
+estimator = tb.Estimator(simulator) # Create an estimator
+estimator.estimate(...) # Run the estimator
+
+"""
+OPTIMIZATION
+"""
+optimizer = tb.Optimizer(simulator) # Create an optimizer
+optimizer.optimize(...) # Run the optimizer
+
+# Plot the results
+tb.plot.plot(...)
+
+
